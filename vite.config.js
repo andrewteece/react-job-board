b1bb97a5dafc2491b11c84a4672931165e830b6e
andrewteece <andrewteece@gmail.com>
2024-09-19 08:44:31 -0500

    setup server proxy in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,21 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    [react()],
     eslintPlugin({
       cache: false,
       include: ['./src/**/*.js', './src/**/*.jsx'],
       exclude: [],
     }),
   ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
