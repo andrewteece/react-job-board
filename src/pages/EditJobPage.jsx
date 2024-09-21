318658d9ab352cc8a5da68e57d85d6c92c4335cb
andrewteece <andrewteece@gmail.com>
2024-09-21 06:44:03 -0500

    update/refactir eslint for prop-types

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import { useState } from 'react';
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
